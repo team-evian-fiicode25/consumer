e283ce848b414549bb695294e2262fb3fbed832e
atomicul <soaremihai2006@gmail.com>
2025-03-10 19:30:24 +0200

    Refactor API module into this repository
    Refactor API module into this repository

---
+++ API/go.mod
@@ -0,0 +1,16 @@
+module github.com/team-evian-fiicode25/consumer/API
+
+go 1.24.0
+
+require (
+	github.com/Khan/genqlient v0.7.0 // indirect
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/alexflint/go-arg v1.4.2 // indirect
+	github.com/alexflint/go-scalar v1.0.0 // indirect
+	github.com/bmatcuk/doublestar/v4 v4.6.1 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.19 // indirect
+	golang.org/x/mod v0.20.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/tools v0.24.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ API/internal/apis/router.go
@@ -0,0 +1,23 @@
+package apis
+
+import (
+	"net/http"
+
+	"github.com/team-evian-fiicode25/consumer/API/internal/handlers"
+	"github.com/team-evian-fiicode25/consumer/API/internal/services"
+)
+
+func SetupRoutes() {
+	mux := http.NewServeMux()
+
+	consumerService := services.NewAuthService("http://localhost:8080/graphql")
+
+	registerAuthRoutes(mux, "/", handlers.NewAuthHandler(consumerService))
+
+	http.ListenAndServe(":8000", mux)
+}
+
+func registerAuthRoutes(mux *http.ServeMux, basePath string, handler *handlers.AuthHandler) {
+	mux.HandleFunc(basePath+"/auth/register", handler.Register)
+	mux.HandleFunc(basePath+"/auth/login", handler.Login)
+}
