ee6f8718b287fac4b8569faba398631335afd9f5
atomicul <soaremihai2006@gmail.com>
2025-03-16 14:32:22 +0200

    Connect the database through the business logic library
    This also fixes a bug where a wrong id was sent in place of the proper session token

--- API/cmd/api/main.go
+++ API/cmd/api/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/team-evian-fiicode25/consumer/API/internal/apis"
+import (
+	"log"
+
+	"github.com/team-evian-fiicode25/business-logic/database"
+
+	"github.com/team-evian-fiicode25/consumer/API/internal/apis"
+)
 
 func main() {
+	err := database.InitDBFromEnv()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	apis.SetupRoutes()
 }
