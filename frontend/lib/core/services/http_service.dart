a38524baba3e77ed4ce8bf8d6e2e743f80dc5988
atomicul <soaremihai2006@gmail.com>
2025-03-30 16:09:53 +0300

    Update backend url

--- frontend/lib/core/services/http_service.dart
+++ frontend/lib/core/services/http_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
-const String defaultUrl = "http://10.0.2.2:8000";
+const String defaultUrl = "https://backend.rideme.freshly.life";
 
 class HttpService {
   final String baseUrl;
