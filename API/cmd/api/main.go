207271d70d1b0608253022198514ee1de4eef720
atomicul <soaremihai2006@gmail.com>
2025-04-12 23:54:43 +0300

    Remove business logic dependency

--- API/cmd/api/main.go
+++ API/cmd/api/main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"log"
-
-	"github.com/team-evian-fiicode25/business-logic/database"
-
 	"github.com/team-evian-fiicode25/consumer/API/internal/apis"
 )
 
 func main() {
-	err := database.InitDBFromEnv()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	apis.SetupRoutes()
 }
